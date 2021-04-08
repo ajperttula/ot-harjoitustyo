6d7825c5a93358f53e38b499a6e937430a4bef75
Antti Perttula <anttiperttula@outlook.com>
2021-04-08 13:22:32 +0300

    Väritä ruudukko, kun palikka osuu alalaitaan

--- src/level.py
+++ src/level.py
@@ -1,14 +1,17 @@
 class Level:
     def __init__(self):
-        self.level = [[0 for j in range(10)] for i in range(20)]
+        self.grid = [[0 for j in range(10)] for i in range(20)]
         self.cell_size = 25
         self.block = None
         self.collision = False
+        self.block_grid_y = 0
+        self.block_grid_x = 4
 
     def move_block(self, delta_x: int):
         new_x = self.block.x + delta_x * self.cell_size
         if self.__block_can_move(new_x):
             self.block.x = new_x
+            self.block_grid_x += delta_x
 
     def __block_can_move(self, new_x):
         if new_x < 20 or new_x > 20+(10-self.block.width)*self.cell_size:
@@ -24,8 +27,18 @@ class Level:
         new_y = self.block.y + self.cell_size
         if new_y <= 20+(20-self.block.height)*self.cell_size:
             self.block.y = new_y
+            self.block_grid_y += 1
         else:
             self.collision = True
+            self.update_grid()
+
+    def update_grid(self):
+        for y in range(self.block.height):
+            for x in range(self.block.width):
+                if self.block.shape[y][x] == 1:
+                    self.grid[self.block_grid_y+y][self.block_grid_x+x] = self.block.color
+        self.block_grid_x = 4
+        self.block_grid_y = 0
 
     def new_block(self, block):
         self.block = block
--- src/render.py
+++ src/render.py
@@ -4,7 +4,7 @@ class Render:
     def __init__(self, display, level):
         display.fill((255,255,255))
         self.__draw_block(display, level)
-        self.__draw_grid(display)
+        self.__draw_grid(display, level)
         self.__init_changes()
 
     def __draw_block(self, display, level):
@@ -13,9 +13,12 @@ class Render:
                 if level.block.shape[i][j] == 1:
                     pygame.draw.rect(display, level.block.color, pygame.Rect(level.block.x+j*25, level.block.y+i*25, 25, 25))
 
-    def __draw_grid(self, display):
+    def __draw_grid(self, display, level):
         for i in range(20):
             for j in range(10):
+                if level.grid[i][j] != 0:
+                    color = level.grid[i][j]
+                    pygame.draw.rect(display, color, pygame.Rect(20+j*25, 20+i*25, 25, 25))
                 pygame.draw.rect(display, (120,120,120), pygame.Rect(20+j*25, 20+i*25, 25, 25), 1)
             
     def __init_changes(self):
