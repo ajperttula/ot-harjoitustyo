727fe0c1d635eb76e0db1ad78c862b61c63de496
Antti Perttula <anttiperttula@outlook.com>
2021-04-15 18:07:02 +0300

    Lisää luokka pace pelinopeuden tahdistamiseen

---
+++ src/pace.py
@@ -0,0 +1,24 @@
+class Pace:
+    def __init__(self):
+        self.__counter = 0
+        self.__integer = 0
+        self.__difficulty = 2
+        self.__fast = False
+
+    def increase_counter(self):
+        if self.__fast:
+            self.__counter += self.__difficulty * 4
+        else:
+            self.__counter += self.__difficulty
+
+    def check_counter(self, fps):
+        if self.__counter // fps > self.__integer:
+            self.__integer += 1
+            return True
+        return False
+
+    def increase_speed(self):
+        self.__fast = True
+
+    def decrease_speed(self):
+        self.__fast = False
