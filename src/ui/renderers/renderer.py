6644ec759bf0cc5ba4a8ece9bacfea3cbc93d203
Antti Perttula <anttiperttula@outlook.com>
2021-05-11 15:50:14 +0300

    Lisää docstringejä

--- src/ui/renderers/renderer.py
+++ src/ui/renderers/renderer.py
@@ -6,7 +6,22 @@ TEXT_COLOR = (0, 0, 0)
 
 
 class Renderer:
+    """Class responsible for rendering the display.
+
+    Atributes:
+        display: Pygame display object.
+        display_height: Display height in pixels.
+        display_width: Display width in pixels.
+        font_small: Pygame font to render small text.
+        font_big: Pygame font to render bigger text.
+        main_menu_button: Pygame rect object to represent a button.
+    """
     def __init__(self, display):
+        """Creates a new renderer and sets attributes.
+
+        Args:
+            display: Pygame display object.
+        """
         self._display = display
         self._display_height = display.get_height()
         self._display_width = display.get_width()
@@ -15,15 +30,38 @@ class Renderer:
         self.main_menu_button = self._create_button(290, 480, 120, 40)
 
     def _draw_background(self):
+        """Fills display background with BG_COLOR.
+        """
         self._display.fill(BG_COLOR)
 
     def _init_changes(self):
+        """Updates the display surface.
+        """
         pygame.display.flip()
 
-    def _create_button(self, pos_x, pos_y, width, height):
+    def _create_button(self, pos_x: int, pos_y: int, width: int, height: int):
+        """Creates a pygame rect object to represent a button.
+
+        Args:
+            pos_x (int): Distance from the left border of the display.
+            pos_y (int): Distance from the top border of the display.
+            width (int): Width of the button in pixels.
+            height (int): Height of the button in pixels.
+
+        Returns:
+            Rect: Pygame rect object.
+        """
         return pygame.Rect(pos_x, pos_y, width, height)
 
-    def _draw_button(self, button, text):
+    def _draw_button(self, button, text: str):
+        """Draws a button to the display surface.
+
+        Text is drawn on the middle of the button.
+
+        Args:
+            button: Pygame rect object.
+            text (str): Text on the button.
+        """
         text = self._font_small.render(text, True, TEXT_COLOR)
         pygame.draw.rect(self._display,
                          (255, 0, 0),
--- src/ui/renderers/ui_renderer.py
+++ src/ui/renderers/ui_renderer.py
@@ -6,11 +6,33 @@ TEXT_COLOR = (0, 0, 0)
 
 
 class UIRenderer(Renderer):
+    """Class responsible for rendering user interface windows.
+
+    Args:
+        Renderer: Inherited renderer class.
+
+    Attributes:
+        new_game_button: Pygame rect object to represent a button.
+        high_scores_button: Pygame rect object to represent a button.
+        exit_button: Pygame rect object to represent a button.
+    """
     def __init__(self, display):
+        """Creates a new ui_renderer and sets attributes.
+
+        Args:
+            display: Pygame display object.
+        """
         super().__init__(display)
         self.__create_buttons()
 
-    def draw(self, page, player_name="", scores=""):
+    def draw(self, page: str, player_name="", scores=[]):
+        """Draws background, buttons, titles and results if any.
+
+        Args:
+            page (str): To determine, which page content to draw.
+            player_name (str, optional): To draw player name. Defaults to "".
+            scores (list, optional): List containing top 10 scores. Defaults to [].
+        """
         self._draw_background()
         if page == "main":
             self.__draw_buttons()
@@ -22,6 +44,8 @@ class UIRenderer(Renderer):
         self._init_changes()
 
     def __create_buttons(self):
+        """Creates main manu buttons.
+        """
         self.new_game_button = self._create_button((self._display_width-200)/2,
                                                    (self._display_height-40)/2,
                                                    200,
@@ -38,11 +62,22 @@ class UIRenderer(Renderer):
                                                40)
 
     def __draw_buttons(self):
+        """Draws main menu buttons.
+        """
         self._draw_button(self.new_game_button, "New game")
         self._draw_button(self.high_scores_button, "High scores")
         self._draw_button(self.exit_button, "Exit")
 
-    def __draw_score_table(self, scores):
+    def __draw_score_table(self, scores: list):
+        """Draws score table.
+
+        Titles are drawn on fixed positions on the screen. Then the
+        score list is iterated and drawn having 30 pixels between
+        each other.
+
+        Args:
+            scores (list): Contains top 10 scores in tuples (rank, player name, score)
+        """
         title = self._font_small.render("TOP 10 Scores", True, TEXT_COLOR)
         self._display.blit(title, ((self._display.get_width()-title.get_width())/2, 30))
 
@@ -69,7 +104,12 @@ class UIRenderer(Renderer):
             score = self._font_small.render(f"{result[2]}", True, TEXT_COLOR)
             self._display.blit(score, (300, pos_y))
 
-    def __draw_text_input(self, player_name):
+    def __draw_text_input(self, player_name: str):
+        """Draws text input box.
+
+        Args:
+            player_name (str): player name, which player is inputting.
+        """
         title = self._font_small.render("Player name:", True, TEXT_COLOR)
         title_position = ((self._display_width-title.get_width())/2,
                           (self._display_height-title.get_height())/2)
