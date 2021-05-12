f87ac1a654223386da129df59969b6aad2d1f6a5
Antti Perttula <anttiperttula@outlook.com>
2021-05-12 13:12:39 +0300

    Lisää värikoodin tarkistus

--- src/config.py
+++ src/config.py
@@ -8,14 +8,35 @@ try:
 except FileNotFoundError:
     pass
 
+
+def check(color: str):
+    """Checks that color code from .env file is valid.
+
+    Args:
+        color (str): color in hex-format.
+
+    Returns:
+        (None/str): None if not valid, color if valid
+    """
+    if not color:
+        return None
+    values = "0123456789ABCDEFabcdef"
+    if len(color) != 7 or color[0] != "#":
+        return None
+    for char in color[1:]:
+        if char not in values:
+            return None
+    return color
+
+
 DATABASE_FILENAME = os.getenv("DATABASE_FILENAME") or "scores.db"
-BG_COLOR = os.getenv("BG_COLOR") or "#EBEBEB"
-TEXT_COLOR = os.getenv("TEXT_COLOR") or "#000000"
-BUTTON_COLOR = os.getenv("BUTTON_COLOR") or "#FF0000"
-GRID_COLOR = os.getenv("GRID_COLOR") or "#7D7D7D"
-BLOCK_COLOR_1 = os.getenv("BLOCK_COLOR_1") or "#FF0000"
-BLOCK_COLOR_2 = os.getenv("BLOCK_COLOR_2") or "#00FF00"
-BLOCK_COLOR_3 = os.getenv("BLOCK_COLOR_3") or "#0000FF"
+BG_COLOR = check(os.getenv("BG_COLOR")) or "#EBEBEB"
+TEXT_COLOR = check(os.getenv("TEXT_COLOR")) or "#000000"
+BUTTON_COLOR = check(os.getenv("BUTTON_COLOR")) or "#FF0000"
+GRID_COLOR = check(os.getenv("GRID_COLOR")) or "#7D7D7D"
+BLOCK_COLOR_1 = check(os.getenv("BLOCK_COLOR_1")) or "#FF0000"
+BLOCK_COLOR_2 = check(os.getenv("BLOCK_COLOR_2")) or "#00FF00"
+BLOCK_COLOR_3 = check(os.getenv("BLOCK_COLOR_3")) or "#0000FF"
 SCREEN_WIDTH = 430
 SCREEN_HEIGHT = 540
 GRID_WIDTH = 10
